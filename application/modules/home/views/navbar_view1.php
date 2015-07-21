bb926f1798e3d971b2c670b003c56b9dd8663bc1
jeremiah <kirikak19@gmail.com>
2015-07-21 09:27:37 +0300

    Manipulating  the navabar

--- application/modules/home/views/navbar_view1.php
+++ application/modules/home/views/navbar_view1.php
@@ -1,27 +1,29 @@
-<div class="container">
-  <div class="row">
-    <div class="col-lg-12 col-md-12">
-      <nav class="header ">
-        <div class="nav-wrapper">
+<div class="row top_navigation" style="height:50px;border:2px solid red;">
+  <div class="col-md-4" style="float:left; margin-left:20px; ">
+    <p>Contact link</p>
+  </div>
+  <div class="col-md-8" style="float:right;">
 
-          <ul class="left hide-on-med-and-down">
-          <li><a href="<?php echo base_url(). 'home/contact'?>"><i class="material-icons left">call</i>
-            <button type="button" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#myModal">COntact Support</button>
-          </a></li>
-           </ul>
-           <ul class= "right hide-on-med-and-down">
+    <nav>
+    <div class="nav-wrapper">
+      <a href="#" class="brand-logo">Logo</a>
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <li><a href="sass.html">Sass</a></li>
+        <li><a href="badges.html">Components</a></li>
+        <li><a href="collapsible.html">JavaScript</a></li>
+      </ul>
+    </div>
+  </nav>
+  
+  </div>
+</div>
+<div class="row logo_search"  style="height:50px; border:2px solid yellow;">
+</div><!-- end of the  row div -->
 
-              <li><a href="<?php echo base_url(). 'home/login'?>"><i class="material-icons right">lock_open</i>Log In</a></li>
-              <li><a href="<?php echo base_url(). 'home/register'?>"><i class="material-icons right">supervisor_account</i>Sign Up</a></li>
-              <li><a href="<?php echo base_url(). ''?>"><i class="material-icons right">shopping_basket</i>WishList</a></li>
-              <li><a href=""><i class="material-icons right">shopping_cart</i>Shopping Bag </a></li>
-          
-            </ul>
-        </div>
-        </nav>
-      </div>
-    </div><!-- end of the  row div -->
-</div> <!-- end of the container div -->
+<div class="row categories" style="height:50px; border:2px solid green;">
+
+</div>
+
 
 
  
