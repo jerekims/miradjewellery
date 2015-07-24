5b11424629a9a83e438a52a74f4e3ea4d10ae455
jeremiah <kirikak19@gmail.com>
2015-07-24 09:39:48 +0300

    changes in the db-just ignore the changes

--- application/modules/home/views/v_home.php
+++ application/modules/home/views/v_home.php
@@ -1,30 +1,30 @@
 <div class="billboard">
-  <div class="row">
-    <div class="slider " ><!--Carousel-->
-    <ul class="slides">
-      <li>
-        <img src="<?php echo base_url().'assets/images/slide1.jpg';?>"> <!-- random image -->
-        
-      </li>
-      <li>
-        <img src="<?php echo base_url().'assets/images/slide2.png';?>"> <!-- random image -->
-        
-      </li>
-      <li>
-        <img src="<?php echo base_url().'assets/images/slide3.jpg';?>"> <!-- random image -->
-        
-      </li>
-      <li>
-        <img src="<?php echo base_url().'assets/images/slide4.jpg';?>"> <!-- random image -->
-        
-      </li>
-      <li>
-        <img src="<?php echo base_url().'assets/images/slide6.jpg';?>"> <!-- random image -->
-        
-      </li>
-    </ul>
-  </div> <!-- End of carousel -->
-</div><!-- end of the row div -->
+    <div class="row">
+      <div class="slider " ><!--Carousel-->
+        <ul class="slides">
+          <li>
+            <img src="<?php echo base_url().'assets/images/slide1.jpg';?>"> <!-- random image -->
+            
+          </li>
+          <li>
+            <img src="<?php echo base_url().'assets/images/slide2.png';?>"> <!-- random image -->
+            
+          </li>
+          <li>
+            <img src="<?php echo base_url().'assets/images/slide3.jpg';?>"> <!-- random image -->
+            
+          </li>
+          <li>
+            <img src="<?php echo base_url().'assets/images/slide4.jpg';?>"> <!-- random image -->
+            
+          </li>
+          <li>
+            <img src="<?php echo base_url().'assets/images/slide6.jpg';?>"> <!-- random image -->
+            
+          </li>
+        </ul>
+       </div> <!-- End of carousel -->
+    </div><!-- end of the row div -->
 </div><!-- end of the billboard  div -->
 
 <div class="product" Style="margin-top:10px;margin-bottom:10px;">
