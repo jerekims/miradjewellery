877c92236291fc7c27f67e3a476d939c54f769b2
Willy Mareka <marekawilly@gmail.com>
2015-07-31 15:31:02 +0300

    Error fixed

--- application/modules/home/controllers/home.php
+++ application/modules/home/controllers/home.php
@@ -32,9 +32,9 @@ class Home extends MY_Controller {
 
     function index()
     {
-        $data[''] = '';
         $data['content_page'] = 'home/v_home';
-        
+        $data['top_navbar1']='home/navbar_view1';
+        $data['main_footer']='home/footer_view1';
         
 
         $this->template->call_home_template($data);
