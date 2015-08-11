031959204990771054fe9508c2a0826513f1e71c
OrreS <Orre.steve@strathmore.edu>
2015-08-11 11:50:31 +0300

    Footer and navbar

--- application/modules/home/views/footer_view1.php
+++ application/modules/home/views/footer_view1.php
@@ -1,16 +1,17 @@
-<div class="footer" style="height:100%;width:100%;height:200px;">
+<div id="footer">
+<div class="container" style="height:100%;width:100%;height:200px;">
 	<div class="row">
-		<div class="four columns " style="height:100%;">
-			<h4>Assistance</h4 style="border:1px  solid green;">
-			<ul>
+		<div class="four columns " >
+			<h6 style="color:white;">ASSISTANCE</h6 >
+			<ul class="list-unstyled">
 				<li><a href="#">Contact Support</a></li>
 				<li><a href="#">About Us</a></li>
 				<li><a href="#">SiteMap</a></li>
 			</ul>
 		</div>
 		<div class="four columns" style="height:100%;">
-			<h4>MiradJewellery</h4>
-			<ul>
+			<h6 style="color:white;">MIRAD JEWELLERY</h6>
+			<ul class="list-unstyled">
 				<li><a href="">Rings</a></li>
 				<li><a href="">Necklaces</a></li>
 				<li><a href="">Watches</a></li>
@@ -18,13 +19,18 @@
 			</ul>
 		</div>
 		<div class="four columns" style="height:100%;">
-			<h5>Contact</h5>
-			MON-FRI (9.00am -5.00pm)
-			0929220292
-			miradjewellery.co.ke
-			<ul>
-				<li></li>
-			</ul>
+			<h6 style="color:white;" >MIRAD EMAIL NEWSLETTER</h6>
+			<form class="navbar-form navbar-left" role="search">
+            <div class="form-group">
+            <input type="text" class="form-control" placeholder="Email address">
+            </div>
+            <button type="submit" class="btn btn-default">Subscribe</button>
+            </form>
+			<ul class="list-unstyled">
+			<li>You can also follow us on: </li>
+			<li>miradjewellery.co.ke</li>
+		    </ul>
+			
 		</div>
 	</div>
 </div>
--- application/modules/home/views/navbar_view1.php
+++ application/modules/home/views/navbar_view1.php
@@ -1,38 +1,34 @@
 
-<div class="row ">
-     <div class="navbar-static top_navigation"><!--navigation-->
-        <nav>
-            <div class="nav-wrapper navigation">
-              <ul class="left hide-on-med-and-down" >
-                <li><a href=""><i class="material-icons left">call</i>Contact Support</a></li>
-              </ul>
-             
-             <ul class= "right hide-on-med-and-down">
-
-                <li><a href="<?php  echo base_url().'home/login'?>"><span class="glyphicon glyphicon-user" aria-hidden="true"></span>Log In/Sign Up</a></li>
-                <li><a href="<?php echo  base_url().'home/register'?>"><span class="glyphicon glyphicon-user" aria-hidden="true"></span></a></li>
-                <li><a href=""><i class="material-icons right">shopping_basket</i>WishList</a></li>
-                <li><a href=""><span class="glyphicon glyphicon-shopping-cart" aria-hidden="true"></span>Shopping Bag</a></li>
-                
-              </ul>
-        </nav>
-      </div>  <!--End of navigation-->
-</div>
-
-  <div class="row">
-  <nav class="navbar navigation" style="border-radius:0px;"><!--Navbar for main items-->
-        <div class="logo left hide-on-med-and-down" >
-          <ul>
-            <li><a href="<?php echo base_url().'home/index'?>">MIRAD</a></li>
-          </ul>   
-        </div>
-
-        <div class="nav-wrapper" class="categories">
-          <ul class="right hide-on-med-and-down">
-          <?php echo $navbarcategory;?> 
-          </ul>
-        </div><!-- end of the nav-wrapper -->
-  </nav><!--End of navbar main items-->
-</div>
 
   
+<div class="row">
+  <nav class="navbar-static-top navbar-inverse">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="<?php echo base_url().'home/index'?>">Mirad</a>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+    
+      </form>
+
+      <ul class="nav navbar-nav navbar-right">
+        
+        <li><a href="<?php  echo base_url().'home/login'?>"> <span class="glyphicon glyphicon-user" aria-hidden="true"></span>   Log In/Sign Up</a></li>
+        <li><a href="#"> <span class="glyphicon glyphicon-earphone" aria-hidden="true"></span>   Contact</a></li>
+        <li><a href="#"> <span class="glyphicon glyphicon-briefcase" aria-hidden="true"></span>   Wishlist</a></li>
+        <li><a href="#"> <span class="glyphicon glyphicon-Shopping-cart" aria-hidden="true"></span> Shopping Bag</a></li>
+       
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
+</div>
